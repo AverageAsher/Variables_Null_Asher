ab5cfa73471d48fc41a0f76f5ac7d36932b6d2e0
Someidiotter <anullowo@gmail.com>
2024-02-20 13:00:59 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            int favoriteNumber = 0;
+            bool isRunning = false, isJumping = false;
+            float grade = 99.99f;
+            const double finalGrade = 88.88;
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(favoriteNumber);
+            Console.WriteLine(isRunning);
+            Console.WriteLine(isJumping);
+            Console.WriteLine(grade);
+            Console.WriteLine(finalGrade);
+
         }
     }
 }
