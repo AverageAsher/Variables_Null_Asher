747bed477c29639906e129d5441d4e92efbd1265
Someidiotter <anullowo@gmail.com>
2024-02-20 13:04:06 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,17 +7,26 @@
             Console.WriteLine("Hello, World!");
 
             int favoriteNumber = 0;
+            // The favorite number //
             bool isRunning = false, isJumping = false;
+            // initializes false for jumping and running //
             float grade = 99.99f;
+            // holds information about the grade //
             const double finalGrade = 88.88;
+            // holds the final grade //
 
             Console.WriteLine("Hello, World!");
             Console.WriteLine(favoriteNumber);
+            // writes to the console //
             Console.WriteLine(isRunning);
+            // writes to the console //
             Console.WriteLine(isJumping);
+            // writes to the console //
             Console.WriteLine(grade);
+            // writes to the console //
             Console.WriteLine(finalGrade);
+            // writes to the console //
 
         }
     }
-}
+}  
